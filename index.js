49efe71cda1fe42876e01c415802313257fe9fe4
Tôn Huế <84238568+tonhue25@users.noreply.github.com>
2023-08-18 10:06:03 +0700

    add morgan

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const path = require('path')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+
 const app = express()
 const port = 3000
 
-app.get('/trang-chu', (req, res) => {
-  res.send('Hello World!')
+app.use(morgan('combined'))
+
+app.engine('handlebars', handlebars())
+app.set('view engine', "handlebars")
+app.set('views', path.join(__dirname,"resources/views"))
+
+app.get('/', (req, res) => {
+  //res.send('Hello World!')
+  res.render("home");
 })
 
 app.listen(port, () => {
