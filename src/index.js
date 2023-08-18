5a1116a53efea8e0f13fc48bd6313b81e2dea9e7
Tôn Huế <84238568+tonhue25@users.noreply.github.com>
2023-08-18 14:50:45 +0700

    add express-handlebars

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
+// const express = require('express')
+// const morgan = require('morgan')
+// const app = express()
+// const port = 3000
+
+// app.use(morgan('combined'))
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
 const express = require('express')
+const path = require('path')
 const morgan = require('morgan')
+//const handlebars = require('express-handlebars')
+
+
+const hbs = require('express-handlebars')
+
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use(morgan('combined'))
 
+app.engine('hbs', hbs.engine({extname: '.hbs'}))
+app.set('view engine', "hbs")
+app.set('views', './src/resources/views')
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render("home");
+})
+
+app.get('/news', (req, res) => {
+  res.render("news");
 })
 
 app.listen(port, () => {
